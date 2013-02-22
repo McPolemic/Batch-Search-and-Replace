1582ef3f926c8eb15a522070d5d1013db2dfcdfa
Adam Lukens <spawn968@gmail.com>
2013-02-22 00:57:37 -0500

    Pulled out the filename generation logic

--- bsr_modules_and_comments.py
+++ bsr_modules_and_comments.py
@@ -16,12 +16,21 @@ def generateOutput(template,csv):
             outputLines[i] = line
             for j in range(0, len(csv[i])):
                 outputLines[i] = outputLines[i].replace(csv[0][j],csv[i][j])
-            if csv[0][0] == '<hostname>' and csv[i][0] != '':
-                outputFile = open(csv[i][0] + '.txt', 'a')
-            else:
-                outputFile = open('output' + str(i) + '.txt','a')
+
+            outputName = determineFileName(csv, i)
+            outputFile = open(outputName, 'a')
+
             outputFile.write(outputLines[i])
             outputFile.close()
+
+def determineFileName(csv, offset):
+    csv_heading = csv[0]
+    csv_target  = csv[offset]
+
+    # If we already have a hostname set, use that
+    if csv_heading[0] == '<hostname' and csv_target[0] != '':
+        return '%s.txt' % csv_target[0]
+    return 'output%s.txt' % offset
 
 def importCSV(CSVFileName):
     """Loads a CSV file CSVFileName and returns a list of lists "sample[x][y]"
